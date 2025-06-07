07bc0955a2249b4be5c0970fb0934753c03bac70
luqmanlang <samtingwong2004@gmail.com>
2025-06-08 04:09:42 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-from webhook.tv_webhook_realtime import start_webhook
+from flask import Flask
+from utils.webhook_receiver import webhook_bp
+
+app = Flask(__name__)
+app.register_blueprint(webhook_bp, url_prefix="/webhook")
+
+@app.route("/")
+def home():
+    return "Bot is running!"
 
 if __name__ == "__main__":
-    print("🔧 JohnAI + TradingView Webhook siap dijalankan.")
-    start_webhook()
+    app.run(debug=True)
