2d9ca39786ef3f9b00967000ea46a3b94f4fc1bf
luqmanlang <samtingwong2004@gmail.com>
2025-06-08 16:50:20 +0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,10 +1,13 @@
 import sys
 import os
 
-# Tambah path ke root projek supaya folder 'utils/' boleh diimport
+# Tambah path ke folder atas supaya boleh import dari utils
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+# Import alert & bot
 from utils.analysis import run_analysis
+from utils.discord_bot import bot  # INI PENTING
 
 if __name__ == "__main__":
     run_analysis()
+    bot.run(os.getenv("DISCORD_BOT_TOKEN"))  # HIDUPKAN BOT
