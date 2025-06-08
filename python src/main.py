ca38b9665040717ff6d7485f9a06dd6a31d9eb9d
luqmanlang <samtingwong2004@gmail.com>
2025-06-08 13:39:10 +0800

    Add files via upload

--- python src/main.py
+++ python src/main.py
@@ -1,19 +1,4 @@
-
-import schedule
-import time
 from utils.analysis import run_analysis
 
-def job_1h():
-    run_analysis("1h")
-
-def job_4h():
-    run_analysis("4h")
-
-schedule.every().hour.at(":00").do(job_1h)
-schedule.every(4).hours.at(":00").do(job_4h)
-
-print("📡 Bot JohnAI sedang berjalan...")
-
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+if __name__ == "__main__":
+    run_analysis()
