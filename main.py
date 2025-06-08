b34283bc8cafebfdb8e71bd18a04bc7af127f013
luqmanlang <samtingwong2004@gmail.com>
2025-06-08 11:46:22 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,15 +1,1 @@
-
-from discord_bot import send_alert
-from utils.api_cycle import get_price
-from utils.indicators import get_indicators
-import time
-
-def run_alert():
-    price, source = get_price()
-    rsi, stochastic = get_indicators()
-    send_alert(price, rsi, stochastic, source)
-
-if __name__ == "__main__":
-    while True:
-        run_alert()
-        time.sleep(3600)  # Setiap 1 jam
+# Main bot file
