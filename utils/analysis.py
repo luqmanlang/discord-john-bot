d213acd7c0b3dfe0c9528ab334c2b231cbfd0d48
luqmanlang <samtingwong2004@gmail.com>
2025-06-08 15:04:38 +0800

    Update analysis.py

--- utils/analysis.py
+++ utils/analysis.py
@@ -1,2 +1,6 @@
+from utils.discord_notifier import send_discord_alert
+
 def run_analysis():
-    print("Running analysis...")
+    message = "✅ Bot John telah aktif dan berjaya jalan fungsi `run_analysis()`"
+    print("📨 Menghantar ke Discord:", message)
+    send_discord_alert(message)
